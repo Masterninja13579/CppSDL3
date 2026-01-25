23df87f9d49dc03c165da275877c0badbdd7a688
Andrew Rogers <58229548+Masterninja13579@users.noreply.github.com>
2026-01-25 13:27:10 -0800

    [Windows] Switched rendering to DX12 for windows
    Vulkan on Windows requires the Vulkan SDK to be installed, so for ease of use just switched the rendering API on Windows to DirectX12

--- sandbox/src/core.h
+++ sandbox/src/core.h
@@ -1,4 +1,32 @@
 #pragma once
 
-#include "vendor.h"
 #include "platform/platform.h"
+
+#include <bgfx/bgfx.h>
+#include <bgfx/platform.h>
+
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_properties.h>
+
+#include <imgui.h>
+#include <imgui_impl_sdl3.h>
+
+#ifdef OS_WINDOWS 
+    #include <windows.h>
+    #include <imgui_impl_dx12.h>
+    #define PLATFORM_SDL_RENDER_FLAG 0
+    #define PLATFORM_BGFX_RENDERERTYPE bgfx::RendererType::Direct3D12;
+#endif
+
+#ifdef OS_MAC
+    #include <imgui_impl_metal.h>
+    #define PLATFORM_SDL_RENDER_FLAG SDL_WINDOW_METAL
+    #define PLATFORM_BGFX_RENDERERTYPE bgfx::RendererType::Metal;
+#endif
+
+#ifdef OS_LINUX 
+    #include "platform/linux/platformdata.h"
+    #include <imgui_impl_vulkan.h>
+    #define PLATFORM_SDL_RENDER_FLAG SDL_WINDOW_VULKAN
+    #define PLATFORM_BGFX_RENDERERTYPE bgfx::RendererType::Vulkan;
+#endif
--- sandbox/src/platform/platform.h
+++ sandbox/src/platform/platform.h
@@ -1,4 +1,4 @@
-#include "vendor.h"
+#pragma once
 
 #if defined(WIN32) || defined(_WIN32) || defined(__Win32__) || defined(__NT__)
     #define OS_WINDOWS 1
@@ -8,21 +8,4 @@
     #define OS_LINUX 1
 #else
     #error "Unsupported Operating System"
-#endif
-
-#ifdef OS_WINDOWS 
-    #include <windows.h>
-    #define PLATFORM_SDL_RENDER_FLAG SDL_WINDOW_VULKAN
-    #define PLATFORM_BGFX_RENDERERTYPE bgfx::RendererType::Vulkan;
-#endif
-
-#ifdef OS_MAC 
-    #define PLATFORM_SDL_RENDER_FLAG SDL_WINDOW_METAL
-    #define PLATFORM_BGFX_RENDERERTYPE bgfx::RendererType::Metal;
-#endif
-
-#ifdef OS_LINUX 
-    #include "platform/linux/platformdata.h"
-    #define PLATFORM_SDL_RENDER_FLAG SDL_WINDOW_VULKAN
-    #define PLATFORM_BGFX_RENDERERTYPE bgfx::RendererType::Vulkan;
 #endif
