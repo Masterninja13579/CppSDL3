62b9902dce74155e67862472fb41fa3bfafeff94
Aaron Rogers <74640676+angrydiplomat@users.noreply.github.com>
2026-02-04 14:00:56 -0700

    Removed BgfxNullCallback struct from windowTest.h

--- sandbox/src/experiments/windowTest.h
+++ sandbox/src/experiments/windowTest.h
@@ -1,26 +1,10 @@
 #pragma once
 
 #include "core.h"
-#include "logo.h"
+#include "bgfxExamples/logo.h"
 #include "window.h"
 
 #include <iostream>
-
-struct BgfxNullCallback : public bgfx::CallbackI
-{
-		virtual void fatal(const char* _filePath, uint16_t _line, bgfx::Fatal::Enum _code, const char* _str) override {}
-		virtual void traceVargs(const char* _filePath, uint16_t _line, const char* _format, va_list _argList) override {}
-		virtual void profilerBegin(const char* _name, uint32_t _abgr, const char* _filePath, uint16_t _line) override {}
-		virtual void profilerBeginLiteral(const char* _name, uint32_t _abgr, const char* _filePath, uint16_t _line) override {}
-		virtual void profilerEnd() override {}
-		virtual uint32_t cacheReadSize(uint64_t _id) override { return 0; }
-		virtual bool cacheRead(uint64_t _id, void* _data, uint32_t _size) override { return false; }
-		virtual void cacheWrite(uint64_t _id, const void* _data, uint32_t _size) override {}
-		virtual void screenShot(const char* _filePath, uint32_t _width, uint32_t _height, uint32_t _pitch, const void* _data, uint32_t _size, bool _yflip) override {}
-		virtual void captureBegin(uint32_t _width, uint32_t _height, uint32_t _pitch, bgfx::TextureFormat::Enum _format, bool _yflip) override {}
-		virtual void captureEnd() override {}
-		virtual void captureFrame(const void* _data, uint32_t _size) override {}
-};
 
 int windowTest()
 {
